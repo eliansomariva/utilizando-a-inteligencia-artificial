e46a1d326d5e836f5692f49f28e2dcc5a5828a5c
eliansomariva <159185075+eliansomariva@users.noreply.github.com>
2025-09-02 11:19:39 -0300

    02/09
    ......................

---
+++ script.js
@@ -0,0 +1,21 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+
+
+const perguntas = [ 
+    {
+        enunciado
+    }
+
+
+
+
+
+
+
+
+]
